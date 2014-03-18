97e88a49920ed22e486d72786773f5ea8043149b
Thibaud Desodt <tibo.desodt@gmail.com>
2014-03-18 18:21:56 +0100

    Fluentish construction of the navigatio

---
+++ MvcSmartNav/Helpers/NavComponentsExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+using MvcSmartNav.Enablement;
+using MvcSmartNav.Visibility;
+
+namespace MvcSmartNav.Helpers
+{
+    public static class NavComponentsExtensions
+    {
+        public static TNavComponent WithToolTip<TNavComponent>(this TNavComponent self, string tooltip) where TNavComponent : NavStaticComponentBase
+        {
+            self.Tooltip = tooltip;
+            return self;
+        }
+
+        public static TNavComponent ShowNever<TNavComponent>(this TNavComponent self)
+            where TNavComponent : NavStaticComponentBase
+        {
+            self.VisibilityStrategy = new NeverVisibleStrategy();
+            return self;
+        }
+
+        public static TNavComponent EnabledNever<TNavComponent>(this TNavComponent self)
+            where TNavComponent : NavStaticComponentBase
+        {
+            self.EnablementStrategy = new AlwaysDisabledStrategy();
+            return self;
+        }
+
+        public static TNavComponent WithChild<TNavComponent>(this TNavComponent self, INavItem child )
+            where TNavComponent : NavStaticComponentBase
+        {
+            self.AddChild(child);
+            return self;
+        }
+
+        public static MvcActionNavItem<TController> WithToolTip<TController>(this MvcActionNavItem<TController> self, string tooltip)
+            where TController : IController
+        {
+            self.Tooltip = tooltip;
+            return self;
+        }
+
+
+        
+    }
+}
--- WebApp/Controllers/NavigationController.cs
+++ WebApp/Controllers/NavigationController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Web.Mvc;
 using MvcSmartNav;
-using MvcSmartNav.Enablement;
-using MvcSmartNav.Visibility;
+using MvcSmartNav.Helpers;
 
 namespace WebApp.Controllers
 {
@@ -14,122 +13,92 @@ namespace WebApp.Controllers
         public ActionResult GenerateNav(ViewContext callingContext)
         {
             var root = new NavStaticRoot("NAVIGATION ROOT (home)", Url.Action("Index", "Home"))
-                              {
-                                  Tooltip = "Home of the web site"
-                              };
-
+                .WithToolTip("Home of the web site");
+               
             // ============== STATIC NAV ITEMS ==========================
-            var staticNavItemCategory = new NavStaticItem("Static items", Url.Action("Index", "Home"))
-                                 {
-                                     Tooltip = "some links to static items",
-                                 };
-            root.AddChild(staticNavItemCategory);
-
-            var staticNavItemChild1 = new NavStaticItem("Some Page", Url.Action("SomePage", "Home"))
-                                  {
-                                      Tooltip = "Some Page - should be active when browsing there !"
-                                  };
-            staticNavItemCategory.AddChild(staticNavItemChild1);
-
-            var staticNavItemChild2 = new NavStaticItem("Not a link")
-            {
-                Tooltip = "I don't have a link !"
-            };
-            staticNavItemCategory.AddChild(staticNavItemChild2);
-
-            var staticNavItemChild2SubItem = new NavStaticItem("Grand-son", Url.Action("Index", "Home"));
-            staticNavItemChild2.AddChild(staticNavItemChild2SubItem);
-
-            var staticNavItemChild2SubItemSubItem = new NavStaticItem("Great Grand-son", "http://www.perdu.com");
-            staticNavItemChild2SubItem.AddChild(staticNavItemChild2SubItemSubItem);
+            root.AddChild(
+                new NavStaticItem("Static items", Url.Action("Index", "Home"))
+                    .WithToolTip("some links to static items")
+                    .WithChild(
+                        new NavStaticItem("Some Page", Url.Action("SomePage", "Home"))
+                            .WithToolTip("Some Page - should be active when browsing there !"))
+                    .WithChild(
+                        new NavStaticItem("Not a link")
+                            .WithToolTip("I don't have a link !")
+                            .WithChild(
+                                new NavStaticItem("Grand-son", Url.Action("Index", "Home"))
+                                    .WithChild(
+                                        new NavStaticItem("Great Grand-son", "http://www.perdu.com")
+                                    )
+                            )
+                    )
+            );
 
 
             // ================= Visibility ===============================
-
-            var visibilityNavCategory = new NavStaticItem("Visibility Features");
-            root.AddChild(visibilityNavCategory);
-
-            var visibitilyNavChild1 = new NavStaticItem("Hidden Home link", Url.Action("Index", "Home"))
-            {
-                Tooltip = "Tooltip of element",
-                VisibilityStrategy = new NeverVisibleStrategy()
-            };
-            visibilityNavCategory.AddChild(visibitilyNavChild1);
-
-            var visibilityNavChild2NeverVisible = new NavStaticItem("Not visible with children")
-                                                      {
-                                                          VisibilityStrategy = new NeverVisibleStrategy()
-                                                      };
-            visibilityNavCategory.AddChild(visibilityNavChild2NeverVisible);
-
-            var childOfNeverVisibleItem = new NavStaticItem("My father is invisible .. ");
-            visibilityNavChild2NeverVisible.AddChild(childOfNeverVisibleItem);
-
-            var childDisabledOfNeverVisibleItem = new NavStaticItem("My father is invisible .. and I'm disabled")
-                                                      {
-                                                          EnablementStrategy = new AlwaysDisabledStrategy()
-                                                      };
-            visibilityNavChild2NeverVisible.AddChild(childDisabledOfNeverVisibleItem);
-
-            var visibilityNavChild2Visible = new NavStaticItem("Visible with hidden children");
-            visibilityNavCategory.AddChild(visibilityNavChild2Visible);
-
-
-            var child1InvisibleOfVisibleItem = new NavStaticItem("I'm invisible")
-                                                  {
-                                                      VisibilityStrategy = new NeverVisibleStrategy()
-                                                  };
-            visibilityNavChild2Visible.AddChild(child1InvisibleOfVisibleItem);
-            var child2InvisibleOfVisibleItem = new NavStaticItem("I'm invisible too")
-            {
-                VisibilityStrategy = new NeverVisibleStrategy()
-            };
-            visibilityNavChild2Visible.AddChild(child2InvisibleOfVisibleItem);
-
+            root.AddChild(
+                new NavStaticItem("Visibility Features")
+                    .WithChild(
+                        new NavStaticItem("Hidden Home link", Url.Action("Index", "Home"))
+                            .WithToolTip("Tooltip of element")
+                            .ShowNever()
+                    )
+                    .WithChild(
+                        new NavStaticItem("Not visible with children")
+                            .ShowNever()
+                            .WithChild(
+                                new NavStaticItem("My father is invisible .. ")
+                            )
+                            .WithChild(
+                                new NavStaticItem("My father is invisible .. and I'm disabled")
+                                .EnabledNever()
+                            )
+                    )
+                    .WithChild(
+                        new NavStaticItem("Visible with hidden children")
+                            .WithChild(
+                                new NavStaticItem("I'm invisible")
+                                    .ShowNever()
+                            )
+                            .WithChild(
+                                new NavStaticItem("I'm invisible too")
+                                    .ShowNever()
+                            )
+                    )
+            );
 
             // Enabled / disabled
-            var enablementNavCategory = new NavStaticItem("Enablement Features");
-            root.AddChild(enablementNavCategory);
-
-            var enablementNavChild1 = new NavStaticItem("Always disabled")
-                                          {
-                                              EnablementStrategy = new AlwaysDisabledStrategy()
-                                          };
-            enablementNavCategory.AddChild(enablementNavChild1);
-
-            var enablementNavChild2 = new NavStaticItem("I'am enabled");
-            enablementNavCategory.AddChild(enablementNavChild2);
-
-            var child1DisabledOfEnabledItem = new NavStaticItem("I am disabled")
-                                                  {
-                                                      EnablementStrategy = new AlwaysDisabledStrategy()
-                                                  };
-            enablementNavChild2.AddChild(child1DisabledOfEnabledItem);
-
-            var child2DisabledOfEnabledItem = new NavStaticItem("I am disabled too")
-            {
-                EnablementStrategy = new AlwaysDisabledStrategy()
-            };
-            enablementNavChild2.AddChild(child2DisabledOfEnabledItem);
+            root.AddChild(
+                new NavStaticItem("Enablement Features")
+                    .WithChild(
+                        new NavStaticItem("Always disabled")
+                            .EnabledNever()
+                    )
+                    .WithChild(
+                        new NavStaticItem("I'am enabled")
+                            .WithChild(
+                                 new NavStaticItem("I am disabled")
+                                    .EnabledNever()
+                            )
+                            .WithChild(
+                                new NavStaticItem("I am disabled too")
+                                    .EnabledNever()
+                            )
+                    )
+            );
 
             // ================= MVC Nav Items =============================
             var mvcNavItemCategory = new NavStaticItem("MvcNav items", Url.Action("Index", "Home"))
-            {
-                Tooltip = "using Mvc specific stuff",
-            };
+                .WithToolTip("using Mvc specific stuff");
             root.AddChild(mvcNavItemCategory);
 
 
             var mvcNavItemChild1 = new MvcActionNavItem<HomeController>("Restricted Page", "SomeRestrictedPage")
-            {
-                Tooltip = "Some super secret Page (will disable if not allowed)"
-            };
+                .WithToolTip("Some super secret Page (will disable if not allowed)");
             mvcNavItemCategory.AddChild(mvcNavItemChild1);
 
             var mvcNavItemChild2 = new MvcActionNavItem<HomeController>("Other Restricted Page", "OtherRestrictedPage")
-            {
-                Tooltip = "Some super secret Page (will hide if not allowed)"
-            };
+            .WithToolTip("Some super secret Page (will hide if not allowed)");
             mvcNavItemCategory.AddChild(mvcNavItemChild2);
 
 
